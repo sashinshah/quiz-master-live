598f1d3f1d21649b705038842f2f0459c5d4b2c1
sashinshah <freeregss@gmail.com>
2025-12-20 02:54:10 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,17 @@
+<script src="https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/8.10.0/firebase-database.js"></script>
+
+<script>
+const firebaseConfig = {
+  apiKey: "AIzaSyBannahmQGBkxroS6Nouc-jaqqVwBPLwTQ",
+  authDomain: "quizmasterlive-2344a.firebaseapp.com",
+  databaseURL: "https://quizmasterlive-2344a-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "quizmasterlive-2344a",
+  storageBucket: "quizmasterlive-2344a.firebasestorage.app",
+  messagingSenderId: "410713115220",
+  appId: "1:410713115220:web:1bec31a5953605dcdd7bc1"
+};
+
+  firebase.initializeApp(firebaseConfig);
+  const database = firebase.database();
+</script>
